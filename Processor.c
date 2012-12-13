10a4f424cf5f218b4b5ae886e78cbe9515740847
Tom Charlton <tcharlton90@googlemail.com>
2012-12-13 22:55:13 +0000

    tidy up

--- Processor.c
+++ Processor.c
@@ -4,14 +4,9 @@
 
 // TODO change read-in hack to strcmp
 //	Branch predictor: not right or wrong, predicted taken or predicted not take
-//	Cant make a read-in super-scaler
-//	Make 32 registers
-//	Move to instructions set V2
-//	Out of order
 //	Register renaming unit
 //	Other features (Rob...)
 //	Hypothesis'
-//	\r stuff and nice formatting
 
 int main (int argc, char *argv[])
 {
